23131dd46642b3aa12a5cf13c94c6d98edead2a9
Phạm Văn Hoàn <60645075+phamhoan2608@users.noreply.github.com>
2022-11-06 23:14:55 +0700

    update redux-saga

--- src/redux-toolkit/configureStore.js
+++ src/redux-toolkit/configureStore.js
@@ -1,6 +1,10 @@
 import {configureStore, combineReducers} from "@reduxjs/toolkit"
 import counterSlice from "./counterSlice";
 import globalSlice from "./globalSlice";
+import createSagaMiddleware from 'redux-saga'
+import logger from "redux-logger";
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware()
 
 const reducer = combineReducers({
   counter: counterSlice,
@@ -9,5 +13,8 @@ const reducer = combineReducers({
 
 const store = configureStore({
   reducer,
+  middleware: (gDM) => gDM().concat(logger, sagaMiddleware)
 })
+
+// sagaMiddleware.run(rootSaga)
 export default store;
